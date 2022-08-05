18717eb7f10f33f68bbb3bfbe386550552e7d0e3
sicknarf <fgacad@gmail.com>
2022-08-05 16:39:32 -0700

    updated deck to class instead of an individual array

--- js/main.js
+++ js/main.js
@@ -1,28 +1,28 @@
 let pHand = []
 let dHand = []
-let deck = []
-function newDeck(){
-    deck = [];
-    const suits = ['s', 'c', 'h', 'd'];
-    const values = ['a', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'j', 'q', 'k'];
-    suits.forEach((suit)=>{
-        values.forEach((value)=>{
-            let card = {suit: suit, value: value};
-            deck.push(card);
+class Deck{
+    constructor(){
+        this.deck = [];
+        const suits = ['s', 'c', 'h', 'd'];
+        const values = ['a', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'j', 'q', 'k'];
+        suits.forEach((suit)=>{
+            values.forEach((value)=>{
+                let card = {suit: suit, value: value};
+                this.deck.push(card);
+            });
         });
-    }); 
-};
-function shuffle(array) { //TIL about the fisher-yates algorithm
-        for(let i = array.length - 1; i > 0; i--){
+        
+        };
+    shuffle(){ //TIL about the fisher-yates algorithm
+        for(let i = this.deck.length - 1; i > 0; i--){
             const j = Math.floor(Math.random() * (i + 1));
-            const temp = array[i];
-            array[i] = array[j];
-            array[j] = temp;
+            const temp = this.deck[i];
+            this.deck[i] = this.deck[j];
+            this.deck[j] = temp;
         }
-    }
-newDeck();
-shuffle(deck);
-console.log(deck);
-newDeck();
-shuffle(deck);
-console.log(deck)
+}
+};
+
+let cards = new Deck
+cards.shuffle();
+console.log(cards.deck);
