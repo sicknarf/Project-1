fda74b73c4f9a987cc463043bd3c3dcfebacd679
sicknarf <fgacad@gmail.com>
2022-08-09 17:50:19 -0700

    re-organized functions to have a more logical order

--- js/main.js
+++ js/main.js
@@ -59,7 +59,6 @@ let discardPile = [];
 let aceCheck = false;
 let currentBet = 0;
 
-// jQuery
 $(function(){ 
     $('#initialize').click(init);
     $('#reset-game').click(init);
@@ -104,11 +103,9 @@ $(function(){
     })
 });
 
-
-// functions below
 function init(){
     $('#playing-space').show();
-    $('#deal').prop('disabled', false);
+    $('#deal, #bet-down, #bet-up, #betting-time, #all-in, #clear-bet').prop('disabled', false);
     setTimeout(()=>{$('#splash').hide()}, '800');
     $('#player-hand, #dealer-hand, #deal-again, #broke').hide();
     $('#splash').addClass('animate__animated animated__slower animate__zoomOut');
@@ -134,140 +131,6 @@ function init(){
     betDisplay();
     $('#bet-amount').html(`${betAmounts[betID]}`);
 }
-
-function scoreCount(player){
-    player.score = 0
-    let handLength = player.hand.length
-    for(let i = 0; i < handLength; i++){
-        player.score = player.score + parseInt(scoreCounter[player.hand[i].cardNum]); console.log('current hand is', player.hand[i])
-    }
-};
-
-function pCount() {
-    aceChecker(p.hand);
-    // p.score = 0;
-    scoreCount(p);
-    if(p.score > 21 && aceCheck === true){
-        let aceCard = p.hand.find(hand => hand.cardNum === 'ace');
-        aceCard.cardNum = 'aceOne';
-        scoreCount(p);
-        };
-    if(p.score === 21 && p.hand.length === 2){
-        $('#game-stats').html('you got Blackjack!');
-        animateGameStats();
-        animateAnnouncementsBox();
-        $('#announcements').html('<img class="animate__animated animate__fadeIn animate__slower animate__delay-1s" src="assets/blackjack.png">');
-        $('#hit').prop('disabled', true);
-    } else if (p.score === 21) {
-        $('#game-stats').html('you got 21!');
-        animateGameStats();
-        $('#hit').prop('disabled', true);
-    } else if (p.score > 21){
-        $('#hit').prop('disabled', true);
-        $('#stay').prop('disabled', true);
-        $('#game-stats').html('BUST');
-        animateGameStats();
-        lose();
-    }
-    setTimeout(()=>{$('#player-hand-count').html(p.score)}, '400');
-};
-
-function aceChecker(hand){
-    aceCheck = hand.some((ace) => {
-        return ace.cardNum === 'ace';
-    })
-};
-
-function animateGameStats() {
-    $('#game-stats').addClass('animate__animated animate__fadeIn animate__slower animate__delay-2s');
-    $('#game-stats').css('background-color', 'rgba(0,0,0,0.5)');
-}
-
-function animateAnnouncementsBox() {
-    $('#announcements-box').addClass('animate__animated animate__fadeIn animate__slower');
-    $('#announcements-box').css('background-color', 'rgba(50,50,50,0.5)')
-}
-
-function dealerAI() {
-    // d.score = 0;
-    scoreCount(d);
-    // $('#dealer-space').html(`<span id="dealer-0"><img class="animate__animated animate__flipInY animate__slower" src="assets/playable-cards/${d.hand[0].cardNum}_of_${d.hand[0].suit}.png"></span><span id="dealer-1"></span>`);
-    $('#game-stats').html('');
-    $('#game-stats').css('background-color', 'rgba(0,0,0,0)');
-    // $('#announcements').html('');
-    $('#hit').prop('disabled', true);
-    $('#stay').prop('disabled', true);
-    aceChecker(d.hand);
-    if (d.score > 21 && aceCheck === true){
-        let aceCard = d.hand.find(hand => hand.cardNum === 'ace');
-        aceCard.cardNum = 'aceOne';
-        dealerAI();
-    }
-    if (d.score >= 17){
-        $('#dealer-1').html(`<img src="assets/playable-cards/${d.hand[1].cardNum}_of_${d.hand[1].suit}.png">`)
-        if(d.score === 21 && d.hand.length === 2 && p.score === 21 && p.hand.length === 2){
-            $('#game-stats').html('both players got Blackjack!');
-            animateGameStats();
-            push();
-        } else if(d.score === 21 && d.hand.length === 2){
-            $('#game-stats').html('the dealer got Blackjack, you did not');
-            animateGameStats();
-            lose();
-        } else if (p.score === 21 && d.score === 21 && p.hand.length === 2) {
-            $('#game-stats').html('you got Blackjack, the dealer did not');
-            animateGameStats();
-            win();
-        } else if(d.score === 21 && p.score === 21) {
-            push()
-            $('#deal-again').show();
-            $('#deal').prop('disabled', false);
-        } else if (d.score > 21){
-            animateGameStats();
-            $('#game-stats').html('dealer bust!');
-            win()
-        } else if (21-d.score === 21-p.score){
-            push()
-        } else if(21-d.score < 21-p.score){
-            lose();
-        } else if(21-d.score > 21-p.score){
-            win();
-        } else {
-            console.log('error. find out what happened.');
-            $('#game-stats').html('an unexpected error has occurred');
-            };
-        $('#dealer-hand-count').html(d.score)
-
-    }
-    if (d.score < 17) {
-        d.cardCounter++;
-        d.cardDelay++;
-        cardPop(d);
-        scoreCount(d);
-        dealerAI();
-        }
-};
-
-function clearHands() {
-    let pHandLength = p.hand.length
-    let dHandLength = d.hand.length
-    for(let i = 0; i < pHandLength; i++){
-        let topPCard = p.hand.pop(i);
-        discardPile.push(topPCard);
-    }
-    for(let j = 0; j < dHandLength; j++){
-        let topDCard = d.hand.pop(j);
-        discardPile.push(topDCard);
-    }
-    if (discardPile.length > cards.deck.length * .33){
-        // console.log('discard pile got large. creating new deck and shuffling.')
-        cards = new Deck
-        cards.shuffle();
-        discardPile = []
-    }
-    // console.log('clearHands end. p.hand is', p.hand, 'd.hand is', d.hand);
-    // console.log('discard pile is', discardPile);
-    // console.log('deck is', cards.deck)
-};
 
 function deal () {
     p.cardCounter = 0;
@@ -307,47 +170,6 @@ function deal () {
     $('#announcements-box').css('background-color', 'rgba(50,50,50,0)')
     $('#dealer-space').html(`<span id="dealer-0"><img class="animate__animated animate__fadeInDownBig" src="assets/cardback.jpeg""></span><span id="${d.name}-${d.cardCounter}"><img class="animate__animated animate__fadeInDownBig" src="assets/playable-cards/${d.hand[d.cardCounter].cardNum}_of_${d.hand[d.cardCounter].suit}.png"></span>`);
 };
-
-function cardPop(player) {
-    player.hand.push(cards.deck.pop());
-    $(`#${player.name}-space`).append(`<span id="${player.name}-${player.cardCounter}"><img class="animate__animated animate__fadeInDownBig animate__delay-${player.cardDelay-1}s" id="cards-${player.name}-${player.cardCounter}" src="assets/playable-cards/${player.hand[player.cardCounter].cardNum}_of_${player.hand[player.cardCounter].suit}.png"></span>`);
-};
-
-function hit() {
-    $('#double').prop('disabled', true);
-    cardPop(p);
-    p.cardCounter++;
-    pCount();
-};
-
-function updateBet() {
-    if(p.chips-betAmounts[betID] >= 0){
-        p.chips = p.chips - betAmounts[betID];
-        currentBet = currentBet + betAmounts[betID];
-        $('#bet-notifications').html('');
-        $('#bet-notifications').css('background-color','rgba(0,0,0,0)');
-    } else if (p.chips-betAmounts[betID] < 0){
-        $('#bet-notifications').css('background-color','pink');
-        $('#bet-notifications').html('you can\'t bet that much!');
-    };
-    betDisplay();
-    $('#bet-amount').html(`${betAmounts[betID]}`);
-    if (p.chips < 1000 && p.chips >= 500 && betID > 5){
-        betID = 5;
-    } else if (p.chips < 500 && betID > 4){
-        betID = 4;
-    } else if (p.chips < 250 && betID > 3){
-        betID = 3;
-    } else if (p.chips < 100 && betID > 2){
-        betID = 2;
-    } else if (p.chips < 25 && betID > 1){
-        betID = 1;
-    } else if (p.chips < 5) {
-        betID = 0;
-    };
-    $('#bet-amount').html(`${betAmounts[betID]}`);
-    
-}
 
 function modifyBet(direction) {
     if (p.chips >= 1000){
@@ -410,6 +232,35 @@ function modifyBet(direction) {
     $('#bet-amount').html(`${betAmounts[betID]}`)
 }
 
+function updateBet() {
+    if(p.chips-betAmounts[betID] >= 0){
+        p.chips = p.chips - betAmounts[betID];
+        currentBet = currentBet + betAmounts[betID];
+        $('#bet-notifications').html('');
+        $('#bet-notifications').css('background-color','rgba(0,0,0,0)');
+    } else if (p.chips-betAmounts[betID] < 0){
+        $('#bet-notifications').css('background-color','pink');
+        $('#bet-notifications').html('you can\'t bet that much!');
+    };
+    betDisplay();
+    $('#bet-amount').html(`${betAmounts[betID]}`);
+    if (p.chips < 1000 && p.chips >= 500 && betID > 5){
+        betID = 5;
+    } else if (p.chips < 500 && betID > 4){
+        betID = 4;
+    } else if (p.chips < 250 && betID > 3){
+        betID = 3;
+    } else if (p.chips < 100 && betID > 2){
+        betID = 2;
+    } else if (p.chips < 25 && betID > 1){
+        betID = 1;
+    } else if (p.chips < 5) {
+        betID = 0;
+    };
+    $('#bet-amount').html(`${betAmounts[betID]}`);
+    
+}
+
 function clearBet() {
     p.chips = p.chips + currentBet;
     currentBet = currentBet - currentBet;
@@ -417,6 +268,162 @@ function clearBet() {
     $('#bet-pool').html(currentBet);
     $('#bet-notifications').html('');
     $('#bet-notifications').css('background-color','rgba(0,0,0,0)');
+}
+
+function betDisplay() {
+    $('#chips-left').html(p.chips);
+    $('#bet-pool').html(currentBet);
+}
+
+function hidePlays() {
+    setTimeout(()=>{$('#hit, #stay, #double').hide()}, '800')
+    $('#hit, #stay, #double').addClass('animate__animated animate__fadeOut');
+}
+
+function showPlays() {
+    $('#hit, #stay, #double').removeClass('animate__fadeOut');
+    $('#hit, #stay, #double').show();
+}
+
+function aceChecker(hand){
+    aceCheck = hand.some((ace) => {
+        return ace.cardNum === 'ace';
+    })
+};
+
+function scoreCount(player){
+    player.score = 0
+    let handLength = player.hand.length
+    for(let i = 0; i < handLength; i++){
+        player.score = player.score + parseInt(scoreCounter[player.hand[i].cardNum]);
+    }
+};
+
+function pCount() {
+    aceChecker(p.hand);
+    // p.score = 0;
+    scoreCount(p);
+    if(p.score > 21 && aceCheck === true){
+        let aceCard = p.hand.find(hand => hand.cardNum === 'ace');
+        aceCard.cardNum = 'aceOne';
+        scoreCount(p);
+        };
+    if(p.score === 21 && p.hand.length === 2){
+        $('#game-stats').html('you got Blackjack!');
+        animateGameStats();
+        animateAnnouncementsBox();
+        $('#announcements').html('<img class="animate__animated animate__fadeIn animate__slower animate__delay-1s" src="assets/blackjack.png">');
+        $('#hit').prop('disabled', true);
+    } else if (p.score === 21) {
+        $('#game-stats').html('you got 21!');
+        animateGameStats();
+        $('#hit').prop('disabled', true);
+    } else if (p.score > 21){
+        $('#hit').prop('disabled', true);
+        $('#stay').prop('disabled', true);
+        $('#game-stats').html('BUST');
+        animateGameStats();
+        lose();
+    }
+    setTimeout(()=>{$('#player-hand-count').html(p.score)}, '400');
+};
+
+function hit() {
+    $('#double').prop('disabled', true);
+    cardPop(p);
+    p.cardCounter++;
+    pCount();
+};
+
+function cardPop(player) {
+    player.hand.push(cards.deck.pop());
+    $(`#${player.name}-space`).append(`<span id="${player.name}-${player.cardCounter}"><img class="animate__animated animate__fadeInDownBig animate__delay-${player.cardDelay-1}s" id="cards-${player.name}-${player.cardCounter}" src="assets/playable-cards/${player.hand[player.cardCounter].cardNum}_of_${player.hand[player.cardCounter].suit}.png"></span>`);
+};
+
+function dealerAI() {
+    scoreCount(d);
+    $('#game-stats').html('');
+    $('#game-stats').css('background-color', 'rgba(0,0,0,0)');
+    $('#hit, #stay').prop('disabled', true);
+    aceChecker(d.hand);
+    if (d.score > 21 && aceCheck === true){
+        let aceCard = d.hand.find(hand => hand.cardNum === 'ace');
+        aceCard.cardNum = 'aceOne';
+        dealerAI();
+    }
+    if (d.score >= 17){
+        $('#dealer-1').html(`<img src="assets/playable-cards/${d.hand[1].cardNum}_of_${d.hand[1].suit}.png">`)
+        if(d.score === 21 && d.hand.length === 2 && p.score === 21 && p.hand.length === 2){
+            $('#game-stats').html('both players got Blackjack!');
+            animateGameStats();
+            push();
+        } else if(d.score === 21 && d.hand.length === 2){
+            $('#game-stats').html('the dealer got Blackjack, you did not');
+            animateGameStats();
+            lose();
+        } else if (p.score === 21 && d.score === 21 && p.hand.length === 2) {
+            $('#game-stats').html('you got Blackjack, the dealer did not');
+            animateGameStats();
+            win();
+        } else if(d.score === 21 && p.score === 21) {
+            push()
+            $('#deal-again').show();
+            $('#deal').prop('disabled', false);
+        } else if (d.score > 21){
+            animateGameStats();
+            $('#game-stats').html('dealer bust!');
+            win()
+        } else if (21-d.score === 21-p.score){
+            push()
+        } else if(21-d.score < 21-p.score){
+            lose();
+        } else if(21-d.score > 21-p.score){
+            win();
+        } else {
+            console.log('error. find out what happened.');
+            $('#game-stats').html('an unexpected error has occurred');
+            };
+        setTimeout(()=>{$('#dealer-hand-count').html(d.score)}, '1000');
+    }
+    if (d.score < 17) {
+        d.cardCounter++;
+        d.cardDelay++;
+        cardPop(d);
+        scoreCount(d);
+        dealerAI();
+        }
+};
+
+function clearHands() {
+    let pHandLength = p.hand.length
+    let dHandLength = d.hand.length
+    for(let i = 0; i < pHandLength; i++){
+        let topPCard = p.hand.pop(i);
+        discardPile.push(topPCard);
+    }
+    for(let j = 0; j < dHandLength; j++){
+        let topDCard = d.hand.pop(j);
+        discardPile.push(topDCard);
+    }
+    if (discardPile.length > cards.deck.length * .33){
+        // console.log('discard pile got large. creating new deck and shuffling.')
+        cards = new Deck
+        cards.shuffle();
+        discardPile = []
+    }
+    // console.log('clearHands end. p.hand is', p.hand, 'd.hand is', d.hand);
+    // console.log('discard pile is', discardPile);
+    // console.log('deck is', cards.deck)
+};
+
+function animateGameStats() {
+    $('#game-stats').addClass('animate__animated animate__fadeIn animate__slower animate__delay-2s');
+    $('#game-stats').css('background-color', 'rgba(0,0,0,0.5)');
+}
+
+function animateAnnouncementsBox() {
+    $('#announcements-box').addClass('animate__animated animate__fadeIn animate__slower');
+    $('#announcements-box').css('background-color', 'rgba(50,50,50,0.5)')
 }
 
 function push() {
@@ -453,19 +460,4 @@ function win() {
     currentBet = 0;
     setTimeout((betDisplay), '1500');
     hidePlays();
-}
-
-function hidePlays() {
-    setTimeout(()=>{$('#hit, #stay, #double').hide()}, '800')
-    $('#hit, #stay, #double').addClass('animate__animated animate__fadeOut');
-}
-
-function showPlays() {
-    $('#hit, #stay, #double').removeClass('animate__fadeOut');
-    $('#hit, #stay, #double').show();
-}
-
-function betDisplay() {
-    $('#chips-left').html(p.chips);
-    $('#bet-pool').html(currentBet);
 }
